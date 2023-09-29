4a65501a5ea8b3b12e4f877e56ff8a43c406883f
Ahmed Soltani <ahmed.soltani2026@gmail.com>
2023-09-29 22:22:28 +0200

    adding the game analytics / top bar

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 import pygame
 pygame.init()
 
-WIDTH, HEIGHT= 600, 400 #might use 800 x 600 for bigger screens
+WIDTH, HEIGHT= 800, 600 #might use 800 x 600 for bigger screens
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Aim Trainer") #name of the window
@@ -15,6 +15,10 @@ TARGET_EVENT = pygame.USEREVENT
 TARGET_PADDING = 30
 
 BG_COLOR = (0, 25, 40) 
+LIVES = 3
+TOP_BAR_HEIGHT = 50
+
+LABEL_FONT = pygame.font.SysFont("comicsans", 24)
 
 class Target:
 	MAX_SIZE = 30
@@ -47,6 +51,7 @@ class Target:
 	def collide(self, x, y):
 		dis = math.sqrt((self.x - x)**2 + (self.y - y)**2)
 		return dis <= self.size
+ 
 
 def draw(win, targets):
 	win.fill(BG_COLOR)
@@ -54,14 +59,36 @@ def draw(win, targets):
 	for target in targets:
 		target.draw(win)
 
-	pygame.display.update() 
+def format_time(secs):
+	milli = math.floor(int(secs * 1000 % 1000) / 100)
+	seconds = int(round(secs % 60, 1))
+	minutes = int(secs // 60)
+
+	return f"{minutes:02d}:{seconds:02d}.{milli}"
+
+def draw_top_bar(win, elapsed_time, targets_pressed, misses):
+	pygame.draw.rect(win, "grey", (0 , 0, WIDTH, TOP_BAR_HEIGHT))
+
+	time_label = LABEL_FONT.render(f"Time: {format_time(elapsed_time)}", 1, "black") # 1 for the antialiasing
+
+	speed = round(targets_pressed / elapsed_time, 1) # to the first decimal
+	speed_label = LABEL_FONT.render(f"Speed: {speed} t/s", 1, "black")
+
+	hits_label = LABEL_FONT.render(f"Hits: {targets_pressed}", 1, "black")
+
+	lives_label = LABEL_FONT.render(f"Lives: {LIVES - misses}", 1, "black")
+
+	win.blit(time_label, (5, 5)) #blit is used to display another surface
+	win.blit(speed_label, (200, 5))
+	win.blit(hits_label, (450, 5))
+	win.blit(lives_label, (650, 5))
 
 def main():
 	run = True
 	targets = []
 	clock = pygame.time.Clock()
 
-	target_pressed = 0
+	targets_pressed = 0
 	clicks = 0
 	misses = 0
 	start_time = time.time()
@@ -72,6 +99,7 @@ def main():
 		clock.tick(60) #60 FPS
 		click = False
 		mouse_pos = pygame.mouse.get_pos()
+		elapsed_time = time.time() - start_time
 
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
@@ -80,7 +108,7 @@ def main():
 
 			if event.type == TARGET_EVENT:
 				x = random.randint(TARGET_PADDING, WIDTH - TARGET_PADDING)
-				y = random.randint(TARGET_PADDING, HEIGHT - TARGET_PADDING)
+				y = random.randint(TARGET_PADDING + TOP_BAR_HEIGHT, HEIGHT - TARGET_PADDING)
 				target = Target(x, y)
 				targets.append(target)
 
@@ -97,11 +125,16 @@ def main():
 
 			if click and target.collide(*mouse_pos): # added the * to break down the tuple to its individual components AKA splat operator
 				targets.remove(target)
-				target_pressed += 1
+				targets_pressed += 1
+
+		if misses >= LIVES:
+			pass # end game
 
 		draw(WIN, targets)
+		draw_top_bar(WIN, elapsed_time, targets_pressed, misses)
+		pygame.display.update() 
 
 	pygame.quit()
 
 if __name__ == "__main__":
-	main()
+	main() 
