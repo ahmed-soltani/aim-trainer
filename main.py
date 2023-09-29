88d233f78c81eb67cc205522102fa7466a0dc9b9
Ahmed Soltani <ahmed.soltani2026@gmail.com>
2023-09-29 20:39:49 +0200

    updates from 28/9/2023

--- main.py
+++ main.py
@@ -9,6 +9,13 @@ WIDTH, HEIGHT= 600, 400 #might use 800 x 600 for bigger screens
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Aim Trainer") #name of the window
 
+TARGET_INCREMENT = 400
+TARGET_EVENT = pygame.USEREVENT
+
+TARGET_PADDING = 30
+
+BG_COLOR = (0, 25, 40) 
+
 class Target:
 	MAX_SIZE = 30
 	GROWTH_RATE = 0.2
@@ -30,21 +37,43 @@ class Target:
 		else:
 			self.size -= self.GROWTH_RATE
 
-def draw(self, win):
-	pygame.draw.circle(win, self.COLOR, (self.x, self.y), self.size)
-	pygame.draw.circle(win, self.SECOND_COLOR, (self.x, self.y), self.size * 0.8)
-	pygame.draw.circle(win, self.COLOR, (self.x, self.y), self.size * 0.6)
-	pygame.draw.circle(win, self.SECOND_COLOR, (self.x, self.y), self.size * 0.4)
+	def draw(self, win):
+		pygame.draw.circle(win, self.COLOR, (self.x, self.y), self.size)
+		pygame.draw.circle(win, self.SECOND_COLOR, (self.x, self.y), self.size * 0.8)
+		pygame.draw.circle(win, self.COLOR, (self.x, self.y), self.size * 0.6)
+		pygame.draw.circle(win, self.SECOND_COLOR, (self.x, self.y), self.size * 0.4)
+		pygame.draw.circle(win, self.COLOR, (self.x, self.y), self.size * 0.2)
 
+def draw(win, targets):
+	win.fill(BG_COLOR)
+
+	for target in targets:
+		target.draw(win)
+
+	pygame.display.update() 
 
 def main():
 	run = True
+	targets = []
+
+	pygame.time.set_timer(TARGET_EVENT, TARGET_INCREMENT) # Trigger event every increment of time
 
 	while run:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
-				run = False
+				run = False 
 				break
+
+			if event.type == TARGET_EVENT:
+				x = random.randint(TARGET_PADDING, WIDTH - TARGET_PADDING)
+				y = random.randint(TARGET_PADDING, HEIGHT - TARGET_PADDING)
+				target = Target(x, y)
+				targets.append(target)
+
+		for target in targets:
+			target.update()
+
+		draw(WIN, targets)
 
 	pygame.quit()
 
